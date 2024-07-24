94eb974c975ec43689637dbadf9d0458017d9b74
Haroun Azoulay <antoine.azoulay@cji.paris>
2024-07-24 12:46:32 +0200

    Add Dockerfile and compose

--- crowdin/src/Controller/LuckyController.php
+++ crowdin/src/Controller/LuckyController.php
@@ -1,9 +1,9 @@
 <?php
 // src/Controller/LuckyController.php
 namespace App\Controller;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 class LuckyController
 {   
     #[Route('/lucky/number')]
