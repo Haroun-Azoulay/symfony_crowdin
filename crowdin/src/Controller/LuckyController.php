68821572ccc564ef75a40f6bdabfccfe92358a6e
Haroun Azoulay <antoine.azoulay@cji.paris>
2024-07-28 18:39:42 +0200

    Add template html-css-js
    Test commit
    
    Resolve merge conflicts

--- crowdin/src/Controller/LuckyController.php
+++ crowdin/src/Controller/LuckyController.php
@@ -6,13 +6,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 class LuckyController
 {   
-    #[Route('/lucky/number')]
-    public function number(): Response
+    #[Route('/signup', name: 'app_signup')]
+    public function signuppage(): Response
     {
-        $number = random_int(0, 100);
-
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('signup.html.twig');
     }
 }
