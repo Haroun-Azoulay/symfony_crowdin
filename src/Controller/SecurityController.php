add5d0ad2a0216f6d5704e598eb32e4eb7c669f8
Haroun-Azoulay <ha.azoulay93@gmail.com>
2024-08-10 12:34:46 +0200

    Clean up authentification logic

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,11 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    #[Route(path: '/connection', name: 'app_connection')]
+    public function connection(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -18,11 +22,46 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', [
+        return $this->render('security/connection.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
     }
+
+    #[Route('/inscription', name: 'app_inscription')]
+    public function inscription(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $error = null;
+
+        if ($request->isMethod('POST')) {
+            $email = $request->request->get('email');
+            $plainPassword = $request->request->get('password');
+
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $error = 'Invalid email address.';
+            } elseif (strlen($plainPassword) < 6) {
+                $error = 'Password must be at least 6 characters long.';
+            } else {
+
+                $user = new User();
+                $user->setEmail($email);
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hashedPassword);
+                $user->setRoles(['ROLE_USER']);
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_connection');
+            }
+        }
+
+        return $this->render('security/inscription.html.twig', [
+            'error' => $error
+        ]);
+    }
+
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
