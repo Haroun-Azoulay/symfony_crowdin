c023932a8f39569abf9b02411b15bf474e175c98
Haroun-Azoulay <ha.azoulay93@gmail.com>
2024-08-16 00:09:41 +0200

    Factory profile logic controller

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Controller;
 
 use App\Entity\Profil;
@@ -12,95 +11,81 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ProfilController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/my-profil', name: 'app_profil')]
-    public function handleProfil(Request $request, EntityManagerInterface $entityManager): Response
+    public function handleProfil(): Response
     {
         $user = $this->getUser();
-        $profil = $entityManager->getRepository(Profil::class)->findOneBy(['user' => $user]);
+        $profil = $this->entityManager->getRepository(Profil::class)->findOneBy(['user' => $user]);
 
-        if ($profil) {
-            return $this->redirectToRoute('app_profil_show', ['id' => $profil->getId()]);
-        } else {
-            return $this->redirectToRoute('app_profil_create');
-        }
+        return $profil
+            ? $this->redirectToRoute('app_profil_show', ['id' => $profil->getId()])
+            : $this->redirectToRoute('app_profil_create');
     }
 
     #[Route('/profil/create', name: 'app_profil_create')]
-    public function createProfil(Request $request, EntityManagerInterface $entityManager): Response
+    public function createProfil(Request $request): Response
     {
         if ($request->isMethod('POST')) {
-            $user = $this->getUser();
-            $description = $request->request->get('description');
-            $languages = $request->request->all('languages');
-            $skills = $request->request->all('skills');
-
-            if (!is_array($languages)) {
-                $languages = [$languages];
-            }
-            if (!is_array($skills)) {
-                $skills = [$skills];
-            }
-
             $profil = new Profil();
-            $profil->setDescription($description);
-            $profil->setLanguages($languages);
-            $profil->setSkills($skills);
-            $profil->setUser($user);
-
-            $entityManager->persist($profil);
-            $entityManager->flush();
-
+            $this->saveProfil($profil, $request);
             return $this->redirectToRoute('app_profil_show', ['id' => $profil->getId()]);
         }
 
-        return $this->render('profil.html.twig');
+        return $this->render('profil/create-profil.html.twig');
     }
 
     #[Route('/profil/{id}', name: 'app_profil_show')]
     public function showProfil(Profil $profil): Response
     {
-        $skills = serialize($profil->getSkills());
-        return $this->render('sow.html.twig', [
+        return $this->render('profil/read-profil.html.twig', [
             'profil' => $profil,
-            'skills' => $skills,
+            'skills' => serialize($profil->getSkills()),
         ]);
     }
+
     #[Route('/profil-update/{id}', name: 'app_profil_update')]
-    public function updateProfil(Request $request, int $id, EntityManagerInterface $entityManager): Response
+    public function updateProfil(Request $request, int $id): Response
     {
-        $profil = $entityManager->getRepository(Profil::class)->find($id);
-    
+        $profil = $this->entityManager->getRepository(Profil::class)->find($id);
+
         if (!$profil) {
             throw $this->createNotFoundException('No profil found for id ' . $id);
         }
-    
+
         if ($request->isMethod('POST')) {
-            $description = $request->request->get('description');
-            $languages = $request->request->all('languages');
-            $skills = $request->request->all('skills');
-    
-            if (!is_array($languages)) {
-                $languages = [$languages];
-            }
-            if (!is_array($skills)) {
-                $skills = [$skills];
-            }
-    
-            $profil->setDescription($description);
-            $profil->setLanguages($languages);
-            $profil->setSkills($skills);
-    
-
-            $entityManager->flush();
-    
-
-            return $this->redirectToRoute('app_profil_show', [
-                'id' => $profil->getId(),
-            ]);
+            $this->saveProfil($profil, $request);
+            return $this->redirectToRoute('app_profil_show', ['id' => $profil->getId()]);
         }
 
-        return $this->render('sasa.html.twig', [
+        return $this->render('profil/update-profil.html.twig', [
             'profil' => $profil,
         ]);
+    }
+
+    private function saveProfil(Profil $profil, Request $request): void
+    {
+        $description = $request->request->get('description');
+        $languages = $this->getRequestArray($request->request->all('languages'));
+        $skills = $this->getRequestArray($request->request->all('skills'));
+
+        $profil->setDescription($description);
+        $profil->setLanguages($languages);
+        $profil->setSkills($skills);
+        $profil->setUser($this->getUser());
+
+        $this->entityManager->persist($profil);
+        $this->entityManager->flush();
+    }
+
+    private function getRequestArray($input): array
+    {
+        return is_array($input) ? $input : [$input];
     }
 }
